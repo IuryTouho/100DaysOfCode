928b4370f0eb5e78e4c873cd392fa011fe722f70
IuryTouho <iurytouho@gmail.com>
2024-04-10 22:09:07 -0300

    Dia-7_10-04-2024
    Setimo dia de aprendizado
    
    - Bot respondendo aos comandos

--- Dia_6_7/bot_telegram/bot.py
+++ Dia_6_7/bot_telegram/bot.py
@@ -1,5 +1,5 @@
+from telegram import Update
 from telegram.ext import *
-import telegram
 import constants,responses
 
 
@@ -9,16 +9,27 @@ def start_command(update,context):
 def help_command(update,context):
     update.message.reply_text("oi olá -> retorna uma saudação\n ")
 
-def handle_message(update,context):
+async def handle_message(update,context):
     txt = str(update.message.text).lower()
     response = responses.sample_response(txt)
 
-    update.message.reply_text(response)
+    await update.message.reply_text(response)
 
 def error(update, context):
     print(f"Update: {update} Contexto: {context.error} ")
 
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
 def main():
+    app = ApplicationBuilder().token(constants.API_KEY).build()
+
+    app.add_handler(CommandHandler("hello", hello))
+    app.add_handler(MessageHandler(filters.TEXT,handle_message))
+    
+
+    app.run_polling()
     """
     updater = Updater(constants.API_KEY, True)
     dp = updater.dispatcher
@@ -31,13 +42,7 @@ def main():
     updater.start_polling()
     updater.idle()
     """
+   
 
-    bot = telegram.Bot(token=constants.API_KEY)
-    
-    bot.dispatcher.add_handler(telegram.CommandHandler("start", start_command))
-    bot.dispatcher.add_handler(telegram.CommandHandler("help",help_command))
-    bot.dispatcher.add_handler(CommandHandler(Filters.text,handle_message))
-    bot.add_error_handler(error)
 
-    bot.start_polling()
 main()
