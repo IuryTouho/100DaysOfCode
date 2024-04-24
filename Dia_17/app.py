63939d8a94d2c037fe0a8c1a65f4014568942fcb
IuryTouho <iurytouho@gmail.com>
2024-04-23 21:07:32 -0300

    Dia-18_20-04-2024
    Décimo Oitavo dia de aprendizado
    
    - Começados sobre maquinas virtuais

--- Dia_17/app.py
+++ Dia_17/app.py
@@ -3,18 +3,23 @@ from bd import Carros
 
 
 app = Flask('Carro')
+app.config['JSON_SORT_KEYS'] = False
 
 @app.route('/carros',methods=['GET'])
 def get_carros():
     return make_response(
-        jsonify(Carros) 
+        #jsonify(Carros)
+        mensagem='lista de carros.',
+        dados=Carros 
     ) 
 
 @app.route('/carros',methods=['POST'])
 def create_carros():
     carro = request.json
     Carros.append(carro)
-    return carro
+    return make_response(
+        jsonify(carro) 
+    ) 
 
 app.run()
 
