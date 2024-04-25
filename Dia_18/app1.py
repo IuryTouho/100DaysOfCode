59c6c4231286294cf5a85aba307a9db84b2517f9
IuryTouho <iurytouho@gmail.com>
2024-04-24 21:54:19 -0300

    Dia-20_24-04-2024
    Vigésimo dia de aprendizado
    
    - Feitos testes com Django

--- Dia_18/app1.py
+++ Dia_18/app1.py
@@ -1,5 +1,5 @@
 # para instalar a maquina virtual python -m venv minha_env
 
-# source minha_env/bin/activate      ativar maquina
+# minha_env/bin/activate      ativar maquina
 
 # deactivate    desativar maquina
