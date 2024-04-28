bb41789fe2a4293850a9977406e6c9a07b396515
IuryTouho <iurytouho@gmail.com>
2024-04-27 21:54:04 -0300

    Dia-23_27-04-2024
    Vigésimo terceiro dia de aprendizado
    
    -Feitos testes com pivy

---
+++ Dia_23/app.py
@@ -0,0 +1,28 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+
+class Teste(App):
+    def build(self):
+        box = BoxLayout()
+        #box = BoxLayout(orientation='vertical')
+        button = Button(text='botão 1')
+        button2 = Button(text='botão 1')
+        label = Label(text='texto 1')
+        box.add_widget(button)
+        box.add_widget(button2)
+        box.add_widget(label)
+
+        box2 = BoxLayout()
+        #box = BoxLayout(orientation='vertical')
+        button3 = Button(text='botão 1')
+        button4 = Button(text='botão 1')
+        label2 = Label(text='texto 1')
+        box.add_widget(button3)
+        box.add_widget(button4)
+        box.add_widget(label2)
+
+        box.add_widget(box2)
+        return box
+Teste().run()
